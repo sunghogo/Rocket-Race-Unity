e45cfe724ce0c59734da3041e721e31951c396c2
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-04-11 12:51:13 -0400

    Rocket now rotates in 3D space

---
+++ Assets/Rocket.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] private float _rotationSpeed = 100f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
+            RotateLeft();
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
+            RotateRight();
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
+            RotateUp();
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
+            RotateDown();
+        }
+    }
+    
+    private void RotateLeft() {
+        transform.Rotate(Vector3.forward * Time.deltaTime * _rotationSpeed);
+    }
+
+    private void RotateRight() {
+        transform.Rotate(Vector3.back * Time.deltaTime * _rotationSpeed);
+    }
+
+    private void RotateUp() {
+        transform.Rotate(Vector3.left * Time.deltaTime * _rotationSpeed);
+    }
+
+    private void RotateDown() {
+        transform.Rotate(Vector3.right * Time.deltaTime * _rotationSpeed);
+    }
+}
